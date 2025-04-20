dd58e5c58c6d6423d59e6d1b518f60d554b2e8aa
DK <ashukr8081@gmail.com>
2025-04-20 12:27:58 +0530

    Update Vite configuration to set server host and add Vercel rewrites configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "https://ptsc-knit.vercel.app/",
     port: 8080,
   },
   plugins: [
